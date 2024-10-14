1f05c5b7d0754865dbccafe5a043efffa759bd84
paket2004 <103316763+paket2004@users.noreply.github.com>
2024-10-14 19:43:43 +0300

    Update dz3.py

--- module_1/dz3.py
+++ module_1/dz3.py
@@ -1,1 +1,14 @@
-
+n = int(input())
+numbers = input()
+numbers = numbers.split(" ")
+arr = []
+for i in range(n):
+    arr.append(int(numbers[i]))
+arr_2 = []
+counter = 0
+for i in range(n):
+    if arr[i] != 0:
+        arr_2.append(arr[i])
+    else:
+        counter += 1
+print(arr_2 + [0] * counter)
